a56f9a05e67377bfaa3d2bc9951e9460f0ae2957
Amaan Siddiqui <amaansiddiqui14@gmail.com>
2024-08-19 20:31:31 +0530

    commit

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,15 @@
 import { connectToDB } from "@/utils/database";
 import Prompt from "@/models/prompt";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async () => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts.reverse()), { status: 200 });
   } catch (e) {
     console.log(e);
     return new Response("Failed to get all prompts.", { status: 500 });
